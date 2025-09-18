373d487466dfab7bc3f4eea92d7ea171d802c414
captisch <Till.Schroeer@web.de>
2025-09-18 13:23:52 +0200

    renaming of Load function. Added Functions to save new configuration file and to search directory path

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -2,8 +2,15 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
+using System.Linq;
 using System.Reflection;
+using System.Threading.Tasks;
+using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Platform.Storage;
+using CommunityToolkit.Mvvm.Input;
 using YamlDotNet.RepresentationModel;
+using YamlDotNet.Serialization.NamingConventions;
 using YamlProcessing.Models;
 
 namespace YamlProcessing.ViewModels;
@@ -14,10 +21,10 @@ public partial class MainWindowViewModel : ViewModelBase
     
     public MainWindowViewModel()
     {
-        Test();
+        Load();
     }
     
-    public void Test()
+    public void Load()
     {
         // Pfad zum YAML-File in Assets-Ordner
         // Build-Eigenschaft der Datei auf "Content" und "Copy if newer" setzen !
@@ -38,11 +45,63 @@ public partial class MainWindowViewModel : ViewModelBase
 
         foreach (var item in items)
         {
-            Console.WriteLine($"Name: {item.Name}, Description: {item.Description}");
+            Console.WriteLine($"Name: {item.Name}, Description: {item.Description}, Access: {item.Access}");
             foreach (var option in item.options)
             {
                 Console.WriteLine($"   {option}");
             }
         }
     }
+    
+    [RelayCommand]
+    private Task Save()
+    {
+        string saveFilePath = Path.Combine(AppContext.BaseDirectory, "Assets", "configFile_output.yaml");
+
+        var yml = "";
+
+        foreach (var item in items)
+        {
+            yml +=  string.Format("{0,-15}", item.Name) + "\t\t\t: " + item.Value + "\n";
+        }
+        
+        /*
+        var serializer = new YamlDotNet.Serialization.SerializerBuilder()
+            .IgnoreFields()
+            .WithNamingConvention(CamelCaseNamingConvention.Instance)
+            .Build();
+        var yml = serializer.Serialize(items);
+        */
+        
+        
+        System.IO.File.WriteAllText(saveFilePath, yml);
+        Console.WriteLine($"Saving to {saveFilePath}");
+        return Task.CompletedTask;;
+    }
+    
+    [RelayCommand]
+    public async Task ChooseOutputDirectory(ConfigItem item)
+    {
+        if (item is null)
+            return;
+
+        var lifetime = Avalonia.Application.Current?.ApplicationLifetime as IClassicDesktopStyleApplicationLifetime;
+        var window = lifetime?.MainWindow;
+        if (window?.StorageProvider is null)
+            return;
+
+        var result = await window.StorageProvider.OpenFolderPickerAsync(new FolderPickerOpenOptions
+        {
+            Title = "Ordner auswählen",
+            AllowMultiple = false
+        });
+
+        var folder = result?.FirstOrDefault();
+        if (folder is not null)
+        {
+            var path = folder.TryGetLocalPath() ?? folder.Path.LocalPath;
+            item.Value = path;
+        }
+    }
+
 }
