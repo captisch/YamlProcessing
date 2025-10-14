c263a01b0bf1669722209ca5300524bf1ea7b4a0
captisch <Till.Schroeer@web.de>
2025-10-14 15:54:32 +0200

    include Module class

---
+++ Models/Module.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace YamlProcessing.Models;
+
+public class Module
+{
+    public string Name { get; set; }
+    public List<Port>? Ports { get; set; }
+    public string? Logic { get; set; } 
+}
+
+public enum PortTypes{
+    none,
+    wire,
+    reg
+}
+
+public enum PortDirections{
+    none,
+    input,
+    output,
+    inout,
+}
+
+public class Port
+{
+    public string Name { get; set; }
+    public int Width { get; set; }
+    public PortTypes Type { get; set; }
+    public PortDirections Direction { get; set; }
+}
+
+public class VerilogParser
+{
+    public List<Module> ReadVerilog(string verilogFile)
+    {
+        var modules = new List<Module>();
+        if (!System.IO.File.Exists(verilogFile)) return modules;
+        if (string.IsNullOrWhiteSpace(verilogFile)) return modules;
+        if (!verilogFile.EndsWith(".v")) return modules;
+        
+        
+        string verilogText = System.IO.File.ReadAllText(verilogFile);
+        
+        var patternSingleLineComment = @"//.*";
+        var patternMultiLineComment = @"/\*[\s\S]*?\*/";
+        
+        var verilogTextNoComments = Regex.Replace(
+            (Regex.Replace (verilogText, patternMultiLineComment, "")),
+            patternSingleLineComment, "");
+
+        var patternModule =
+            @"(?<module>module\s+(?<modulename>\w+)\s*\((?<portlist>[\s\S]+?)\)\s*;(?<logic>[\s\S]*?)endmodule)";
+
+        var matchModule = Regex.Matches(verilogTextNoComments, patternModule);
+
+        foreach (Match match in matchModule)
+        {
+            var modulename = match.Groups["modulename"].Value;
+            var modulelogic = match.Groups["logic"].Value;
+            var moduleports = new List<Port>();
+
+            var portlist = match.Groups["portlist"].Value;
+            var ports = portlist.Split(',')
+                .Select(p => p.Trim())
+                .Where(p => !string.IsNullOrWhiteSpace(p));
+
+            PortDirections lastPortDirection = PortDirections.none;
+            PortTypes lastPortType = PortTypes.none;
+            int lastPortWidth = 1;
+
+            foreach (var port in ports)
+            {
+                var portBracketsReplaced = port.Replace('[', ' ').Replace(']', ' ');
+
+                var elements = portBracketsReplaced.Split(' ')
+                    .Select(p => p.Trim())
+                    .Where(p => !string.IsNullOrWhiteSpace(p));
+
+                foreach (var element in elements)
+                {
+                    if (Enum.TryParse(element, out PortDirections portDirection))
+                    {
+                        lastPortDirection = portDirection;
+                        lastPortType = PortTypes.wire;
+                        lastPortWidth = 1;
+                    }
+                    else if (Enum.TryParse(element, out PortTypes portType))
+                    {
+                        lastPortType = portType;
+                        lastPortWidth = 1;
+                    }
+                    else if (char.IsDigit(element[0]))
+                    {
+                        var width = element.Split(':')
+                            .Select(p => p.Trim())
+                            .Where(p => !string.IsNullOrWhiteSpace(p))
+                            .Select(int.Parse)
+                            .First();
+                        lastPortWidth = width + 1;
+                    }
+                    else
+                    {
+                        moduleports.Add(new Port
+                        {
+                            Direction = lastPortDirection,
+                            Type = lastPortType,
+                            Width = lastPortWidth,
+                            Name = element
+                        });
+                    }
+                }
+            }
+            
+            modules.Add(new Module
+            {
+                Name = modulename,
+                Ports = moduleports,
+                Logic = modulelogic
+            });
+        }
+        return modules;
+    }
+    
+}
