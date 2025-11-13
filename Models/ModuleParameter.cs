879c377af26acd39d3b3d01aba72dec35d5e1fa4
captisch <Till.Schroeer@web.de>
2025-11-13 15:58:59 +0100

    added a class for module parameters

---
+++ Models/ModuleParameter.cs
@@ -0,0 +1,9 @@
+﻿namespace YamlProcessing.Models;
+
+// class for parameters of modules
+// todo: create read method
+public class ModuleParameter
+{
+    public string Name { get; set; }
+    public string Value { get; set; }
+}
