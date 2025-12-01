fe0f58daf2e5b6e2192f7e7b34beb192cadddbf4
captisch <Till.Schroeer@web.de>
2025-12-01 18:28:06 +0100

    removed ModuleParameter.cs class and moved it to Module.cs class

--- Models/ModuleParameter.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace YamlProcessing.Models;
-
-// class for parameters of modules
-// todo: create read method
-public class ModuleParameter
-{
-    public string Name { get; set; }
-    public string Value { get; set; }
-}
